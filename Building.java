836703c42065cf525f3761289cb42ea8670ffd5d
= 'cflemi03 <= 'cflemi03@dcs.bbk.ac.uk>
2014-12-05 13:16:31 +0000

    try/catch input for numFloors

--- Building.java
+++ Building.java
@@ -1,5 +1,16 @@
 package elevator;
 
-public class Building {
+import java.util.Scanner;
 
+public class Building { 	
+	{
+		try {
+			Scanner in = new Scanner(System.in);
+			String input = in.next();
+			int numFloors = Integer.parseInt(input);
+		}
+		catch (NumberFormatException exception) {
+			System.out.println("Input was not a valid number.");
+		}
+	}
 }
